79d9db13f1495561d6737ed6eb73894e80d6b145
Kosol Komenek <kosolkomenek@Kosols-MacBook-Air.local>
2023-11-16 22:30:05 +0700

    update prettier

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App";
 // import registerServiceWorker from './registerServiceWorker';
-import 'tachyons';
+import "tachyons";
 
-const root = createRoot(document.getElementById('root'));
+const root = createRoot(document.getElementById("root"));
 
-root.render(<App />)
+root.render(<App />);
 
 // If using React version lower than 18:
 // ReactDOM.render(<App />, document.getElementById('root'));
